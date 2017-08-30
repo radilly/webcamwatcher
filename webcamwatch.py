b3f6ca38eae55979da6cd607139b6e32035cca13
Bob Dilly <GitHub@radilly.org>
2017-08-30 13:08:46 -0400

    systemd seems to be having an issue with urllib.urlopen(), but not sure why.  Snapshot of the working copy (except the restart issue) for reference.

--- webcamwatch.py
+++ webcamwatch.py
@@ -4,17 +4,36 @@
 # watchdog running on a Pi that will detect when images stopped uploading
 # from my webcam, and then power-cycle the sucker.
 #
+# Invoke with ...  python -u ./webcamwatch.py 2>&1 | tee -a webcamwatch.txt
+#
 # https://stackoverflow.com/questions/22676/how-do-i-download-a-file-over-http-using-python
 # Inverted signals to use the NC side of the relay...
 #
+# https://stackoverflow.com/questions/21662783/linux-tee-is-not-working-with-python
+# The -u option works fine.  However, sys.stdout.flush() wouldn't depend on
+# the command line.  My concern though, on a Pi, is that without buffering
+# you're going to hammer the SD Card over time.  Buffering might help, or
+# reducing the output.
+#
+# ========================================================================================
+# 20170711 RAD Wired up the relay board to AC and got this working with the hardware
+#              by overwriting N_Since_Updated.txt to trip the power cycle.
+#
+# 20170712 RAD Want this to run silently (eventually), but log periodically soas
+#              bot to hammer the SD card.
+# ========================================================================================
 
 import urllib
 import re
+import datetime
 import RPi.GPIO as GPIO
 from time import sleep
+import sys
 
 Relay_channel = [17]
-sleep_for = 150
+sleep_for = 300
+sleep_on_recycle = 600
+log_stride = 12
 
 def setup():
 	GPIO.setmode(GPIO.BCM)
@@ -35,36 +54,61 @@ def setup():
 	'''
 
 def power_cycle():
-	print '...Relay channel %d on' % 1
-	print '...open leftmost pair of connectors.'
+	##DEBUG## print '...Relay channel %d on' % 1
+	##DEBUG## print '...open leftmost pair of connectors.'
+	logger( '...open leftmost pair of connectors.')
 	GPIO.output(17, GPIO.LOW)
-	sleep(2.5)
-	print '...Relay channel %d off' % 1
-	print '...close leftmost pair of connectors.'
+	sleep(5)
+	##DEBUG## print '...Relay channel %d off' % 1
+	##DEBUG## print '...close leftmost pair of connectors.'
+	logger('...close leftmost pair of connectors.')
 	GPIO.output(17, GPIO.HIGH)
 
 def destroy():
-	print "\nShutting down..."
+	##DEBUG## print "\nShutting down..."
+	logger("Shutting down...\n")
 	GPIO.output(17, GPIO.HIGH)
 	GPIO.cleanup()
 
+def logger(message):
+	timestamp = datetime.datetime.utcnow().strftime("%Y%m%d %H:%M:%S")
+	FH = open("./webcamwatch.txt", "a")
+	FH.write(timestamp + " " + message)
+	FH.close
+
 def main():
+	iii = 0
+	##DEBUG## print "Staring " + sys.argv[0] + "\n"
+	logger("Staring " + sys.argv[0] + "\n")
 	while True:
 		response = urllib.urlopen('http://dillys.org/wx/N_Since_Updated.txt')
 		content = response.read()
-		result = re.search('(\d*) .*', content)
-		result = re.search('(\d*) .* ([0-9:]*)_UTC.*', content)
-		## print result.group(1)
+		# ------------------------------------------------------------------
+		# The file contains at least a trailing newline ... I've not looked
+		#   "545   1504095902   12:25:02_UTC "
+		#
+		#     Maybe content = "0 0 00:00:00_UTC" if urlopen fails??
+		# ------------------------------------------------------------------
 		words = re.split(' +', content)
-		## print result.group(1), words[2]
-		print result.group(1), result.group(2), " UTC"
-		interval = int(result.group(1))
-		## print content
+		##DEBUG## print words[0], words[2]
+
+		# Periodically put a record into the log for reference.
+		if 0 == iii % log_stride:
+			logger(words[0] + " " + words[2] + "\n")
+		iii += 1
+
+		##### interval = int(result.group(1))
+		interval = int(words[0])
 		if interval > 3000:
 			power_cycle()
-			# The the cam time to reset, and the webserver crontab to fire
-			sleep(600)
-		sleep(sleep_for)
+			logger(words[0] + " " + words[2] + " power cycled\n")
+			# Give the cam time to reset, and the webserver crontab to fire.
+			# The camera comes up pretty quickly, but it seems to resynch to
+			# the 5-minute interval, and the server crontab only fires every
+			# 5 minutes (unsyncronized as a practical matter).  So 10 min max.
+			sleep(sleep_on_recycle)
+		else:
+			sleep(sleep_for)
 
 if __name__ == '__main__':
 	setup()
