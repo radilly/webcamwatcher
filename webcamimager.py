9c47ff88594825b01e3d80a410948ac281139a32
Bob Dilly <GitHub@radilly.org>
2018-08-13 17:08:50 -0400

    A portion of the log was being written into the directory we check to (temporarily) when creating the tar file.  Now we force this_script to have a full path even if the script is launched from ./ ...

--- webcamimager.py
+++ webcamimager.py
@@ -230,8 +230,12 @@ remote_dir = ""
 image_age_URL = ""
 
 this_script = sys.argv[0]
+if re.match('^\./', this_script) :
+	this_script = "{}/{}".format( os.getcwd(), re.sub('^\./', '', this_script) )
+
 image_data_file = re.sub('\.py', '__.dat', this_script)
 logger_file = re.sub('\.py', '.log', this_script)
+
 
 # Real mtime will always be larger
 last_image_dir_mtime = 0.0
@@ -1646,7 +1650,7 @@ if __name__ == '__main__':
 	#### This might be useful...
 	#### if sys.argv[1] = "stop"
 	log_string( "\n\n\n\n" )
-	log_and_message("INFO: Starting {}   PID={}".format( this_script,getpid() ) )
+	log_and_message("INFO: Starting {}   PID={}".format( this_script, getpid() ) )
 
 ###		For testing @@@
 ###	global ftp_login
