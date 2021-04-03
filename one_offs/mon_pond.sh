02feb5a44e83b5449714f92c6e446caee33f1e8f
Bob Dilly <GitHub@radilly.org>
2021-04-03 09:53:27 -0400

    Playing with doing multiple attempts to ping, so we tolerate network glitches.

--- one_offs/mon_pond.sh
+++ one_offs/mon_pond.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
-# 20210331 Embedded ping in a loop as it was failing once in a while
+# --------------------------------------------------------------------------------
+# NOTE: Example crontab...
+# m h  dom mon dow   command
+#
+# 0,15,30,45 * * * * /home/pi/webcamwatcher/one_offs/mon_pond.sh >> /home/pi/mon_pond.log
+# --------------------------------------------------------------------------------
+# 20210331 Embedded ping in a loop as it was failing once in a while.
 # If you pipe through grep directly, it appears the RC is from grep.
+# --------------------------------------------------------------------------------
+
+host="192.168.1.166"
 
 ITER=8
 RC=-1
 while [[ ${ITER} -gt 0  &&  ${RC} -ne 0 ]] ; do
-#	echo "ITER = ${ITER}  and  RC = ${RC}"
-	ITER=$( expr ${ITER} - 1 )
-	PING_OUT=`ping -c 1 192.168.1.166` ; RC=$?
+	PING_OUT=`ping -c 1 ${host}` ; RC=$?
+	echo "ITER = ${ITER}  and  RC = ${RC}"
+#################################	ITER=$( expr ${ITER} - 1 )
+	(( ITER-- ))
 	if [[ ${RC} -ne 0 ]] ; then
 		/home/pi/webcamwatcher/gpio_driver.py 16 hi > /dev/null
+		echo "$PING_OUT" | egrep 'packets transmitted'
 		date +'%T %F Status: Problem <<<<<<<<<'
 	else
 		/home/pi/webcamwatcher/gpio_driver.py 16 lo > /dev/null
+		echo "$PING_OUT" | egrep '(bytes of data.|packets transmitted)'
 		date +'%T %F Status: OK'
 		sleep 3
 		break
@@ -24,21 +36,4 @@ echo
 
 exit
 
-
-
-
-PING_OUT=`ping -c 1 192.168.1.166` ; RC=$?
-echo "$PING_OUT" | egrep '(bytes of data.|packets transmitted)'
-# echo "RC = ${RC}"
-if [[ ${RC} -ne 0 ]] ; then
-	/home/pi/webcamwatcher/gpio_driver.py 16 hi > /dev/null
-	date +'%T %F Status: Problem <<<<<<<<<'
-else
-	/home/pi/webcamwatcher/gpio_driver.py 16 lo > /dev/null
-	date +'%T %F Status: OK'
-fi
-
-echo
-
-exit
 
